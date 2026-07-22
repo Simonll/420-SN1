0d29a91ab0a35d664d87f314c3266733ef191699
sll <evol.simon@gmail.com>
2026-07-21 23:34:42 -0400

    s3

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 const config = {
     nom: "420-SN1 - Programmation en sciences",
-    description: "Site du cours 420-SN1 Programmation en sciences donné par Simon Laurin-Lemay.",
+    description: "Site du cours 420-SN1 Programmation en sciences adapté par Simon Laurin-Lemay.",
     nomUrl: "420-SN1"
 }
 
