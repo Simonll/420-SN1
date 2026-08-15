d772a1291b10225e24cf0d4b3caf8576d1efd789
simon.laurin-lemay <simon.laurin-lemay@proton.me>
2026-08-15 13:02:38 -0400

    modification page not found

--- web/src/theme/NotFound/Content/index.js
+++ web/src/theme/NotFound/Content/index.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
+
 export default function NotFoundContent({className}) {
   return (
     <main className={clsx('container margin-vert--xl', className)}>
       <div className="row">
-        <div className="col col--6 col--offset-3">
+        <div className="col col--8 col--offset-2">
           <Heading as="h1" className="hero__title">
-            <Translate
-              id="theme.NotFound.title"
-              description="The title of the 404 page">
-              Page Not Found
-            </Translate>
+            Contenu non disponible
           </Heading>
           <p>
-            <Translate
-              id="theme.NotFound.p1"
-              description="The first paragraph of the 404 page">
-              We could not find what you were looking for.
-            </Translate>
+            Le matériel que vous cherchez n'existe pas ou <strong>n'a pas encore été publié</strong>.
           </p>
           <p>
-            <Translate
-              id="theme.NotFound.p2"
-              description="The 2nd paragraph of the 404 page">
-              Please contact the owner of the site that linked you to the
-              original URL and let them know their link is broken.
-            </Translate>
+            Si vous cherchez l'énoncé d'un travail pratique ou les notes d'une leçon future, il est tout à fait normal que cette page soit inaccessible pour le moment. Le contenu sera dévoilé au moment prévu dans le plan de cours.
           </p>
+          <div className="margin-top--lg">
+            <Link className="button button--primary button--lg" to="/">
+              Retourner à l'accueil du cours
+            </Link>
+          </div>
         </div>
       </div>
     </main>
